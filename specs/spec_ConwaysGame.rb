62bac186d745751916688468fe7e98e60d4240ef
Andri Suranta Ginting <andris.ginting@gmail.com>
2018-06-28 20:25:25 +0700

    add initialize array spec

--- specs/spec_ConwaysGame.rb
+++ specs/spec_ConwaysGame.rb
@@ -6,4 +6,10 @@ class TestNAME < Test::Unit::TestCase
   def test_sample
     assert_equal(4, 2+2)
   end
+
+  def test_initiliaze_array_size
+    cells_array = Array.new
+    assert_equal(true,cells_array.empty?)
+  end
+
 end
